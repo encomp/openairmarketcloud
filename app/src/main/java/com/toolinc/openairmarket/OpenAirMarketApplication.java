6a222782b1847a4f9d45d15bdd2ea805b84e1191
encomp <edgar.martinez.rico@gmail.com>
2019-04-30 20:20:43 -0700

    Setting up Timber.

--- app/src/main/java/com/toolinc/openairmarket/OpenAirMarketApplication.java
+++ app/src/main/java/com/toolinc/openairmarket/OpenAirMarketApplication.java
@@ -18,6 +18,7 @@ import javax.inject.Inject;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
+import timber.log.Timber;
 
 /** Base class for maintaining global application state and provide injection to the entire app. */
 public class OpenAirMarketApplication extends Application implements HasActivityInjector {
@@ -45,6 +46,9 @@ public class OpenAirMarketApplication extends Application implements HasActivity
         .inject(this);
     WorkManager.initialize(
         this, new Configuration.Builder().setWorkerFactory(workerFactory).build());
+    if (BuildConfig.DEBUG) {
+      Timber.plant(new Timber.DebugTree());
+    }
   }
 
   @Override
