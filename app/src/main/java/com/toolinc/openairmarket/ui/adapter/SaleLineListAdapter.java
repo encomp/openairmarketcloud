fe274150147a0595d9b0933f1e62d4caa25b40cc
encomp <edgar.martinez.rico@gmail.com>
2019-05-04 12:30:49 -0700

    Minor issue.

--- app/src/main/java/com/toolinc/openairmarket/ui/adapter/SaleLineListAdapter.java
+++ app/src/main/java/com/toolinc/openairmarket/ui/adapter/SaleLineListAdapter.java
@@ -4,35 +4,21 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import com.toolinc.openairmarket.databinding.ItemSaleLineBinding;
 import com.toolinc.openairmarket.pos.persistence.model.sale.SaleLine;
-import com.toolinc.openairmarket.viewmodel.ReceiptViewModel;
-
-import javax.inject.Inject;
+import com.toolinc.openairmarket.viewmodel.ReceiptViewModel.ProductLine;
 
 /**
  * Adapter that provides a binding from an {@link ImmutableList} of {@link SaleLine} to the view
  * {@code R.layout.item_sale_line} displayed within a RecyclerView.
  */
 public final class SaleLineListAdapter
-    extends RecyclerView.Adapter<SaleLineListAdapter.SaleLineViewHolder>
-    implements Observer<ImmutableList<SaleLine>> {
+    extends RecyclerView.Adapter<SaleLineListAdapter.SaleLineViewHolder> {
 
-  private static final ImmutableList<SaleLine> EMPTY = ImmutableList.copyOf(Lists.newArrayList());
-  private final ReceiptViewModel receiptViewModel;
-  private ImmutableList<SaleLine> saleLines = EMPTY;
-
-  @Inject
-  public SaleLineListAdapter(ReceiptViewModel receiptViewModel) {
-    this.receiptViewModel = receiptViewModel;
-    receiptViewModel.getLines().observeForever(this::onChanged);
-  }
+  private ImmutableList<ProductLine> productLines = ImmutableList.of();
 
   @NonNull
   @Override
@@ -44,21 +30,20 @@ public final class SaleLineListAdapter
 
   @Override
   public void onBindViewHolder(@NonNull SaleLineViewHolder saleLineViewHolder, int position) {
-    saleLineViewHolder.itemSaleLineBinding.setSaleLine(saleLines.get(0));
+    saleLineViewHolder.itemSaleLineBinding.setSaleLine(productLines.get(position).saleLine());
   }
 
   @Override
   public int getItemCount() {
-    return saleLines.size();
+    return productLines.size();
   }
 
-  @Override
-  public void onChanged(ImmutableList<SaleLine> saleLines) {
-    if (Optional.fromNullable(saleLines).isPresent()) {
-      this.saleLines = saleLines;
-    } else {
-      this.saleLines = EMPTY;
-    }
+  public ImmutableList<ProductLine> getProductLines() {
+    return productLines;
+  }
+
+  public void setProductLines(@NonNull ImmutableList<ProductLine> productLines) {
+    this.productLines = productLines;
   }
 
   /** Describes a {@link SaleLine} item about its place within the RecyclerView. */
