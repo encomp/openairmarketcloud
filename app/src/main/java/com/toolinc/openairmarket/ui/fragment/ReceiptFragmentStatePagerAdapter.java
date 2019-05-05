40adb587d5388ab8e757cb1cf28d63257714c780
encomp <edgar.martinez.rico@gmail.com>
2019-05-04 20:02:52 -0700

    Fixing tab adapter.

--- app/src/main/java/com/toolinc/openairmarket/ui/fragment/ReceiptFragmentStatePagerAdapter.java
+++ app/src/main/java/com/toolinc/openairmarket/ui/fragment/ReceiptFragmentStatePagerAdapter.java
@@ -1,5 +1,9 @@
 package com.toolinc.openairmarket.ui.fragment;
 
+import android.content.Context;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -9,54 +13,63 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.toolinc.openairmarket.R;
 import com.toolinc.openairmarket.pos.persistence.model.product.Product;
 import com.toolinc.openairmarket.viewmodel.ReceiptViewModel;
 
 /** Implementation of {@link PagerAdapter} that renders the content of three tickets. */
-public final class ReceiptFragmentStatePagerAdapter extends FragmentStatePagerAdapter
-    implements TabLayout.OnTabSelectedListener {
+public final class ReceiptFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
 
+  @StringRes private static final int TAB_1 = R.string.receipt_tab_1;
+  @StringRes private static final int TAB_2 = R.string.receipt_tab_2;
+  @StringRes private static final int TAB_3 = R.string.receipt_tab_3;
+  @DrawableRes private static final int ICON_TAB = R.drawable.ic_receipt;
+
+  private final Context context;
   private final ViewPager viewPager;
   private final TabLayout tabLayout;
   private final ImmutableList<ReceiptFragment> receiptFragments =
       ImmutableList.of(new ReceiptFragment(), new ReceiptFragment(), new ReceiptFragment());
 
   public ReceiptFragmentStatePagerAdapter(
-      FragmentManager fragmentManager, ViewPager viewPager, TabLayout tabLayout) {
+      FragmentManager fragmentManager, Context context, ViewPager viewPager, TabLayout tabLayout) {
     super(fragmentManager);
+    this.context = Preconditions.checkNotNull(context, "Context is missing.");
     this.viewPager = Preconditions.checkNotNull(viewPager, "ViewPager is missing.");
     this.tabLayout = Preconditions.checkNotNull(tabLayout, "TabLayout is missing.");
     viewPager.setAdapter(this);
-    tabLayout.addOnTabSelectedListener(this);
-  }
-
-  @Override
-  public Fragment getItem(int position) {
-    switch (position) {
-      case 0:
-      case 1:
-      case 2:
-        return receiptFragments.get(position);
-
-      default:
-        return null;
+    tabLayout.setupWithViewPager(viewPager);
+    for (int i = 0; i < tabLayout.getTabCount(); i++) {
+      tabLayout.getTabAt(i).setIcon(ICON_TAB);
     }
   }
 
   @Override
-  public void onTabSelected(TabLayout.Tab tab) {
-    viewPager.setCurrentItem(tab.getPosition());
+  public Fragment getItem(int position) {
+    return receiptFragments.get(position);
   }
 
   @Override
-  public void onTabUnselected(TabLayout.Tab tab) {}
-
-  @Override
-  public void onTabReselected(TabLayout.Tab tab) {}
-
-  @Override
   public int getCount() {
-    return 3;
+    return receiptFragments.size();
+  }
+
+  @Override
+  public CharSequence getPageTitle(int position) {
+    switch (position) {
+      case 0:
+        return context.getString(TAB_1);
+
+      case 1:
+        return context.getString(TAB_2);
+
+      case 2:
+        return context.getString(TAB_3);
+
+      default:
+        break;
+    }
+    return "";
   }
 
   public void addProduct(Product product) {
