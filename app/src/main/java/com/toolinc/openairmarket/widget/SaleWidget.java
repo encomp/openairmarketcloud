1995f9fee4a0ca3c913d5c8034b04aaf75c3911a
encomp <edgar.martinez.rico@gmail.com>
2019-05-26 13:03:41 -0700

    Performs the sale total update.

--- app/src/main/java/com/toolinc/openairmarket/widget/SaleWidget.java
+++ app/src/main/java/com/toolinc/openairmarket/widget/SaleWidget.java
@@ -5,34 +5,44 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.toolinc.openairmarket.OpenAirMarketApplication;
 import com.toolinc.openairmarket.R;
 import com.toolinc.openairmarket.persistence.cloud.SaleRepository;
 
+import java.math.BigDecimal;
+
 import javax.inject.Inject;
+
+import timber.log.Timber;
 
 /** Implementation of App Widget functionality. */
 public class SaleWidget extends AppWidgetProvider {
 
-  private Context context;
+  private static final String TAG = SaleWidget.class.getSimpleName();
   @Inject SaleRepository saleRepository;
 
-  static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-    CharSequence widgetText = context.getString(R.string.appwidget_text);
+  static void updateAppWidget(
+      Context context,
+      AppWidgetManager appWidgetManager,
+      int appWidgetId,
+      SaleRepository saleRepository) {
     // Construct the RemoteViews object
-    RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sale_widget);
-    views.setTextViewText(R.id.appwidget_text, widgetText);
+    new SaleAmount(context, appWidgetManager, appWidgetId, saleRepository).updateSale();
     // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views);
+    appWidgetManager.updateAppWidget(
+        appWidgetId, new RemoteViews(context.getPackageName(), R.layout.sale_widget));
   }
 
   @Override
   public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
     // There may be multiple widgets active, so update all of them
-    this.context = context;
     inject(context);
     for (int appWidgetId : appWidgetIds) {
-      updateAppWidget(context, appWidgetManager, appWidgetId);
+      updateAppWidget(context, appWidgetManager, appWidgetId, saleRepository);
     }
   }
 
@@ -50,5 +60,44 @@ public class SaleWidget extends AppWidgetProvider {
   @Override
   public void onDisabled(Context context) {
     // Enter relevant functionality for when the last widget is disabled
+  }
+
+  /** Performs the update of the sale amount. */
+  private static class SaleAmount implements OnSuccessListener<BigDecimal>, OnFailureListener {
+
+    private Context context;
+    private AppWidgetManager appWidgetManager;
+    private int appWidgetId;
+    private SaleRepository saleRepository;
+
+    public SaleAmount(
+        Context context,
+        AppWidgetManager appWidgetManager,
+        int appWidgetId,
+        SaleRepository saleRepository) {
+      this.context = context;
+      this.appWidgetManager = appWidgetManager;
+      this.appWidgetId = appWidgetId;
+      this.saleRepository = saleRepository;
+    }
+
+    public void updateSale() {
+      Timber.tag(TAG).d("About to retrieve the sale amount.");
+      saleRepository.retrieveSalesTotal(24, this, this);
+    }
+
+    @Override
+    public void onSuccess(BigDecimal bigDecimal) {
+      Timber.tag(TAG).d("Sale amount $[%s].", bigDecimal.toPlainString());
+      RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.sale_widget);
+      remoteViews.setTextViewText(
+          R.id.tv_sale_amount, OpenAirMarketApplication.toString(bigDecimal));
+      appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+      Timber.tag(TAG).d("Sale amount failed [%s].", e.getMessage());
+    }
   }
 }
