a01912a7b4272451f6203155382d418e67bea0d8
encomp <edgar.martinez.rico@gmail.com>
2019-05-26 17:57:38 -0700

    Fix Main Fragment to display the email.

--- app/src/main/java/com/toolinc/openairmarket/ui/fragment/MainFragment.java
+++ app/src/main/java/com/toolinc/openairmarket/ui/fragment/MainFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,88 +18,89 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseUser;
 import com.toolinc.openairmarket.R;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import dagger.android.support.DaggerFragment;
 
-/** Initial screen for the pos app. */
+/**
+ * Initial screen for the pos app.
+ */
 public class MainFragment extends DaggerFragment
-    implements OnBackPressedHandler, NavigationView.OnNavigationItemSelectedListener {
+        implements OnBackPressedHandler, NavigationView.OnNavigationItemSelectedListener {
 
-  DrawerLayout drawer;
+    DrawerLayout drawer;
 
-  @BindView(R.id.toolbar)
-  Toolbar toolbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
-  @BindView(R.id.bottom_app_bar)
-  BottomAppBar bottomAppBar;
+    @BindView(R.id.bottom_app_bar)
+    BottomAppBar bottomAppBar;
 
-  @BindView(R.id.fab_add_to_receipt)
-  FloatingActionButton floatingActionButton;
+    @BindView(R.id.fab_add_to_receipt)
+    FloatingActionButton floatingActionButton;
 
-  @Nullable
-  @Override
-  public View onCreateView(
-      LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
+    @Inject
+    FirebaseUser currentUser;
 
-    View view = layoutInflater.inflate(R.layout.fragment_main, viewGroup, false /* attachToRoot */);
-    ButterKnife.bind(this, view);
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
 
-    AppCompatActivity activity = (AppCompatActivity) getActivity();
-    toolbar.setTitle("POS");
-    activity.setSupportActionBar(toolbar);
-    drawer = (DrawerLayout) activity.findViewById(R.id.drawer_layout);
-    ActionBarDrawerToggle toggle =
-        new ActionBarDrawerToggle(
-            activity,
-            drawer,
-            bottomAppBar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close);
-    drawer.addDrawerListener(toggle);
-    toggle.syncState();
+        View view = layoutInflater.inflate(R.layout.fragment_main, viewGroup, false /* attachToRoot */);
+        ButterKnife.bind(this, view);
 
-    // TODO (edgar): Refactor this to extends from FullScreenFragment.
-    ReceiptsFragment receiptsFragment = new ReceiptsFragment();
-    getChildFragmentManager()
-        .beginTransaction()
-        .add(R.id.full_screen_fragment_container, receiptsFragment)
-        .commit();
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        toolbar.setTitle("POS");
+        activity.setSupportActionBar(toolbar);
+        drawer = (DrawerLayout) activity.findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(
+                        activity,
+                        drawer,
+                        bottomAppBar,
+                        R.string.navigation_drawer_open,
+                        R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-    NavigationView navigationView = (NavigationView) activity.findViewById(R.id.nav_view);
-    navigationView.setNavigationItemSelectedListener(this);
-    return view;
-  }
+        NavigationView navigationView = activity.findViewById(R.id.nav_view);
+        TextView tvEmail = navigationView.getHeaderView(0).findViewById(R.id.tv_email);
+        tvEmail.setText(currentUser.getEmail());
 
-  public boolean onBackPressed() {
-    if (drawer.isDrawerOpen(GravityCompat.START)) {
-      drawer.closeDrawer(GravityCompat.START);
-      return true;
+        // TODO (edgar): Refactor this to extends from FullScreenFragment.
+        ReceiptsFragment receiptsFragment = new ReceiptsFragment();
+        getChildFragmentManager()
+                .beginTransaction()
+                .add(R.id.full_screen_fragment_container, receiptsFragment)
+                .commit();
+
+        navigationView.setNavigationItemSelectedListener(this);
+        return view;
     }
-    return false;
-  }
 
-  @SuppressWarnings("StatementWithEmptyBody")
-  @Override
-  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-    // Handle navigation view item clicks here.
-    int id = item.getItemId();
-    if (id == R.id.nav_camera) {
-      // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
-
-    } else if (id == R.id.nav_slideshow) {
-
-    } else if (id == R.id.nav_manage) {
-
-    } else if (id == R.id.nav_share) {
-
-    } else if (id == R.id.nav_send) {
-
+    public boolean onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+        return false;
     }
-    drawer.closeDrawer(GravityCompat.START);
-    return true;
-  }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_log_out) {
+            getActivity().finish();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
