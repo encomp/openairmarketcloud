750b0231c94a0eb17f288383e96c7750cf894d34
Edgar Rico <edgar.martinez.rico@gmail.com>
2019-10-27 09:43:32 -0700

    Define a new testing rule for rx2java.

---
+++ app/src/androidTest/java/com/toolinc/openairmarket/common/reactivex/TrampolineSchedulerRule.java
@@ -0,0 +1,43 @@
+package com.toolinc.openairmarket.common.reactivex;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.plugins.RxJavaPlugins;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.TestScheduler;
+
+public class TrampolineSchedulerRule implements TestRule {
+
+  private final TestScheduler testScheduler = new TestScheduler();
+
+  public TestScheduler getTestScheduler() {
+    return testScheduler;
+  }
+
+  @Override
+  public Statement apply(final Statement base, Description d) {
+    return new Statement() {
+      @Override
+      public void evaluate() throws Throwable {
+        RxJavaPlugins.setIoSchedulerHandler(
+                scheduler -> testScheduler);
+        RxJavaPlugins.setComputationSchedulerHandler(
+                scheduler -> testScheduler);
+        RxJavaPlugins.setNewThreadSchedulerHandler(
+                scheduler -> testScheduler);
+        RxAndroidPlugins.setMainThreadSchedulerHandler(
+                scheduler -> Schedulers.trampoline());
+
+        try {
+          base.evaluate();
+        } finally {
+          RxJavaPlugins.reset();
+          RxAndroidPlugins.reset();
+        }
+      }
+    };
+  }
+}
