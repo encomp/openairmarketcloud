2994eac00ce71b6cbf2a045fa820d1adbaffc165
Edgar Rico <edgar.martinez.rico@gmail.com>
2019-10-27 09:56:57 -0700

    Add pagination functionality to the ProductCategory DAO.

--- app/src/androidTest/java/com/toolinc/openairmarket/persistence/local/pos/ProductRoomCategoryDaoTest.java
+++ app/src/androidTest/java/com/toolinc/openairmarket/persistence/local/pos/ProductRoomCategoryDaoTest.java
@@ -2,25 +2,35 @@ package com.toolinc.openairmarket.persistence.local.pos;
 
 import android.content.Context;
 
+import androidx.paging.PagedList;
+import androidx.paging.RxPagedListBuilder;
 import androidx.room.Room;
-import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.web.internal.deps.guava.collect.ImmutableList;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.google.common.collect.Lists;
+import com.toolinc.openairmarket.common.reactivex.TrampolineSchedulerRule;
 import com.toolinc.openairmarket.persistence.local.pos.dao.ProductRoomCategoryDao;
 import com.toolinc.openairmarket.persistence.local.pos.model.ProductRoomCategory;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.reactivex.Completable;
 import io.reactivex.Maybe;
+import io.reactivex.Observable;
+
+import static com.google.common.truth.Truth.assertThat;
 
 /** Tests for {@link ProductRoomCategoryDao}. */
 @RunWith(AndroidJUnit4.class)
 public class ProductRoomCategoryDaoTest {
+
+  @Rule public final TrampolineSchedulerRule schedulerRule = new TrampolineSchedulerRule();
 
   private static final String[] PRODUCTS = new String[] {"A", "B", "C", "D", "E"};
   private static final ProductRoomCategory.Builder builder =
@@ -35,9 +45,28 @@ public class ProductRoomCategoryDaoTest {
 
   @Before
   public void setUp() {
-    Context context = ApplicationProvider.getApplicationContext();
-    db = Room.inMemoryDatabaseBuilder(context, PosRoomDatabase.class).build();
+    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    db =
+        Room.inMemoryDatabaseBuilder(context, PosRoomDatabase.class)
+            .allowMainThreadQueries()
+            .build();
     productRoomCategoryDao = db.productRoomCategoryDao();
+  }
+
+  @Test
+  public void all_records() {
+    ImmutableList<ProductRoomCategory> products = create();
+    for (ProductRoomCategory productCategory : products) {
+      Completable completable = productRoomCategoryDao.insert(productCategory);
+      completable.test().assertComplete();
+    }
+
+    PagedList.Config config = new PagedList.Config.Builder().setPageSize(10).build();
+    Observable<PagedList<ProductRoomCategory>> result =
+        new RxPagedListBuilder(productRoomCategoryDao.all(), config).buildObservable();
+    PagedList<ProductRoomCategory> productRoomCategories = result.blockingFirst();
+    assertThat(Lists.newArrayList(productRoomCategories.iterator()))
+        .containsAtLeastElementsIn(products);
   }
 
   @Test
@@ -77,6 +106,35 @@ public class ProductRoomCategoryDaoTest {
 
     Maybe<ProductRoomCategory> maybe = productRoomCategoryDao.findById(productCategory.id());
     maybe.test().assertValue(productCategory);
+  }
+
+  @Test
+  public void findById_referenceId() {
+    ProductRoomCategory productCategory = builder.build();
+
+    Completable completable = productRoomCategoryDao.insert(productCategory);
+    completable.test().assertComplete();
+
+    Maybe<ProductRoomCategory> maybe =
+        productRoomCategoryDao.findByReferenceId(productCategory.referenceId());
+    maybe.test().assertValueCount(1);
+  }
+
+  @Test
+  public void findAllLikeName_records() {
+    ImmutableList<ProductRoomCategory> products = create();
+    for (ProductRoomCategory productCategory : products) {
+      Completable completable = productRoomCategoryDao.insert(productCategory);
+      completable.test().assertComplete();
+    }
+
+    PagedList.Config config = new PagedList.Config.Builder().setPageSize(10).build();
+    Observable<PagedList<ProductRoomCategory>> result =
+        new RxPagedListBuilder(productRoomCategoryDao.findAllLikeName("NAME -"), config)
+            .buildObservable();
+    PagedList<ProductRoomCategory> productRoomCategories = result.blockingFirst();
+    assertThat(Lists.newArrayList(productRoomCategories.iterator()))
+        .containsAtLeastElementsIn(products);
   }
 
   @After
--- app/src/main/java/com/toolinc/openairmarket/persistence/local/pos/dao/ProductRoomCategoryDao.java
+++ app/src/main/java/com/toolinc/openairmarket/persistence/local/pos/dao/ProductRoomCategoryDao.java
@@ -1,5 +1,6 @@
 package com.toolinc.openairmarket.persistence.local.pos.dao;
 
+import androidx.paging.DataSource;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -20,6 +21,15 @@ public interface ProductRoomCategoryDao {
   @Query("DELETE FROM ProductCategory WHERE id = :id")
   Completable delete(String id);
 
+  @Query("SELECT * FROM ProductCategory ORDER BY id DESC")
+  DataSource.Factory<Integer, ProductRoomCategory> all();
+
   @Query("SELECT * FROM ProductCategory WHERE id = :id")
   Maybe<ProductRoomCategory> findById(String id);
+
+  @Query("SELECT * FROM ProductCategory WHERE referenceId = :referenceId")
+  Maybe<ProductRoomCategory> findByReferenceId(String referenceId);
+
+  @Query("SELECT * FROM ProductCategory WHERE name LIKE '%' || :name || '%'")
+  DataSource.Factory<Integer, ProductRoomCategory> findAllLikeName(String name);
 }
