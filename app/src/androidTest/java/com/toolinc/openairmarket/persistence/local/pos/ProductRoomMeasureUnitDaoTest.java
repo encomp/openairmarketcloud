7b18423c9f08e1d913515c5d80d5917265ff466d
Edgar Rico <edgar.martinez.rico@gmail.com>
2019-10-27 13:22:21 -0700

    Adding all missing test cases.

--- app/src/androidTest/java/com/toolinc/openairmarket/persistence/local/pos/ProductRoomMeasureUnitDaoTest.java
+++ app/src/androidTest/java/com/toolinc/openairmarket/persistence/local/pos/ProductRoomMeasureUnitDaoTest.java
@@ -3,11 +3,17 @@ package com.toolinc.openairmarket.persistence.local.pos;
 import android.content.Context;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.LiveData;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+import androidx.paging.RxPagedListBuilder;
 import androidx.room.Room;
 import androidx.test.espresso.web.internal.deps.guava.collect.ImmutableList;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.google.common.collect.Lists;
+import com.toolinc.openairmarket.common.lifecycle.LiveDataTestUtil;
 import com.toolinc.openairmarket.common.reactivex.TrampolineSchedulerRule;
 import com.toolinc.openairmarket.persistence.local.pos.dao.ProductRoomMeasureUnitDao;
 import com.toolinc.openairmarket.persistence.local.pos.model.ProductRoomMeasureUnit;
@@ -19,6 +25,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+
+import static com.google.common.truth.Truth.assertThat;
 
 /** Tests for {@link ProductRoomMeasureUnitDao}. */
 @RunWith(AndroidJUnit4.class)
@@ -57,6 +67,115 @@ public class ProductRoomMeasureUnitDaoTest {
 
     Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
     completable.test().assertComplete();
+  }
+
+  @Test
+  public void insert_records() {
+    ImmutableList<ProductRoomMeasureUnit> products = create();
+    for (ProductRoomMeasureUnit productRoomMeasureUnit : products) {
+      Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+      completable.test().assertComplete();
+    }
+  }
+
+  @Test
+  public void delete_record() {
+    ProductRoomMeasureUnit productRoomMeasureUnit = builder.build();
+
+    Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+    completable.test().assertComplete();
+
+    completable = productRoomMeasureUnitDao.delete(productRoomMeasureUnit);
+    completable.test().assertComplete();
+  }
+
+  @Test
+  public void update_record() {
+    ProductRoomMeasureUnit productRoomMeasureUnit = builder.build();
+
+    Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+    completable.test().assertComplete();
+
+    completable =
+        productRoomMeasureUnitDao.update(
+            productRoomMeasureUnit.toBuilder().setName("Other").build());
+    completable.test().assertComplete();
+
+    Maybe<ProductRoomMeasureUnit> maybe =
+        productRoomMeasureUnitDao.findById(productRoomMeasureUnit.id());
+    maybe.test().assertValue(productRoomMeasureUnit.toBuilder().setName("Other").build());
+  }
+
+  @Test
+  public void all_records() {
+    ImmutableList<ProductRoomMeasureUnit> products = create();
+    for (ProductRoomMeasureUnit productRoomMeasureUnit : products) {
+      Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+      completable.test().assertComplete();
+    }
+
+    PagedList.Config config = new PagedList.Config.Builder().setPageSize(10).build();
+    Observable<PagedList<ProductRoomMeasureUnit>> result =
+        new RxPagedListBuilder(productRoomMeasureUnitDao.all(), config).buildObservable();
+    PagedList<ProductRoomMeasureUnit> productRoomMeasureUnits = result.blockingFirst();
+    assertThat(Lists.newArrayList(productRoomMeasureUnits.iterator()))
+        .containsAtLeastElementsIn(products);
+  }
+
+  @Test
+  public void all_records_withLiveData() throws InterruptedException {
+    ImmutableList<ProductRoomMeasureUnit> products = create();
+    for (ProductRoomMeasureUnit productRoomMeasureUnit : products) {
+      Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+      completable.test().assertComplete();
+    }
+
+    PagedList.Config config = new PagedList.Config.Builder().setPageSize(10).build();
+    LiveData<PagedList<ProductRoomMeasureUnit>> listLiveData =
+        new LivePagedListBuilder<>(productRoomMeasureUnitDao.all(), config).build();
+    PagedList<ProductRoomMeasureUnit> pagedList = LiveDataTestUtil.blocking(listLiveData);
+    assertThat(Lists.newArrayList(pagedList.iterator())).containsAtLeastElementsIn(products);
+  }
+
+  @Test
+  public void findById_record() {
+    ProductRoomMeasureUnit productRoomMeasureUnit = builder.build();
+
+    Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+    completable.test().assertComplete();
+
+    Maybe<ProductRoomMeasureUnit> maybe =
+        productRoomMeasureUnitDao.findById(productRoomMeasureUnit.id());
+    maybe.test().assertValue(productRoomMeasureUnit);
+  }
+
+  @Test
+  public void findById_referenceId() {
+    ProductRoomMeasureUnit productRoomMeasureUnit = builder.build();
+
+    Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+    completable.test().assertComplete();
+
+    Maybe<ProductRoomMeasureUnit> maybe =
+        productRoomMeasureUnitDao.findByReferenceId(productRoomMeasureUnit.referenceId());
+    maybe.test().assertValueCount(1);
+  }
+
+  @Test
+  public void findAllLikeName_records() {
+    ImmutableList<ProductRoomMeasureUnit> products = create();
+    for (ProductRoomMeasureUnit productRoomMeasureUnit : products) {
+      Completable completable = productRoomMeasureUnitDao.insert(productRoomMeasureUnit);
+      completable.test().assertComplete();
+    }
+
+    PagedList.Config config = new PagedList.Config.Builder().setPageSize(10).build();
+    Observable<PagedList<ProductRoomMeasureUnit>> result =
+        new RxPagedListBuilder(productRoomMeasureUnitDao.findAllLikeName("NAME -"), config)
+            .buildObservable();
+    PagedList<ProductRoomMeasureUnit> productRoomMeasureUnits = result.blockingFirst();
+    assertThat(Lists.newArrayList(productRoomMeasureUnits.iterator()))
+        .containsAtLeastElementsIn(products);
   }
 
   @After
