95302ad87535422c8717c120a10f3d1af5ed5098
Edgar Rico <edgar.martinez.rico@gmail.com>
2019-11-09 22:58:38 -0800

    Fix the big decimal converter.

--- app/src/main/java/com/toolinc/openairmarket/common/persistence/room/converter/BigDecimalConverter.java
+++ app/src/main/java/com/toolinc/openairmarket/common/persistence/room/converter/BigDecimalConverter.java
@@ -4,13 +4,27 @@ import androidx.room.TypeConverter;
 
 import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.text.ParseException;
+
+import timber.log.Timber;
 
 /** Converts back and forth between {@link BigDecimal} and long data types. */
 public class BigDecimalConverter {
 
+  private static final String TAG = BigDecimalConverter.class.getSimpleName();
+
   @TypeConverter
   public static BigDecimal toBigDecimal(String amount) {
-    return amount == null ? null : new BigDecimal(amount);
+    BigDecimal bigDecimal = null;
+    if (amount != null) {
+      try {
+        Number number = NumberFormat.getCurrencyInstance().parse(amount);
+        bigDecimal = BigDecimal.valueOf(number.doubleValue());
+      } catch (ParseException exc) {
+        Timber.tag(TAG).w("Unable to transform " + amount);
+      }
+    }
+    return bigDecimal;
   }
 
   @TypeConverter
