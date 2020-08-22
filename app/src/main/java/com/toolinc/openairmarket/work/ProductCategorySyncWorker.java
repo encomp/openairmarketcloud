8ef8a7b564f86295969fa13656671fcfd610c617
encomp <edgar.martinez.rico@gmail.com>
2020-08-22 16:38:29 -0700

    Fix the dao.

--- app/src/main/java/com/toolinc/openairmarket/work/ProductCategorySyncWorker.java
+++ app/src/main/java/com/toolinc/openairmarket/work/ProductCategorySyncWorker.java
@@ -9,9 +9,14 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import com.toolinc.openairmarket.persistence.cloud.CollectionsNames;
+import com.toolinc.openairmarket.persistence.cloud.SyncRepository;
 import com.toolinc.openairmarket.persistence.inject.Annotations.Product.Categories;
 import com.toolinc.openairmarket.persistence.local.offline.CollectionSyncStateRepository;
+import com.toolinc.openairmarket.persistence.local.pos.dao.ProductRoomCategoryDao;
+import com.toolinc.openairmarket.persistence.local.pos.model.ProductRoomCategory;
 import com.toolinc.openairmarket.persistence.sync.DataSync;
+import com.toolinc.openairmarket.pos.persistence.model.product.ProductCategory;
+import java.util.List;
 
 /** Downloads the product category information from firestore database. */
 public class ProductCategorySyncWorker extends Worker {
@@ -21,16 +26,19 @@ public class ProductCategorySyncWorker extends Worker {
   private final Context context;
   private final CollectionSyncStateRepository collectionProductRepo;
   private final DataSync dataSync;
+  private final ProductRoomCategoryDao productRoomCategoryDao;
 
   @WorkerInject
   public ProductCategorySyncWorker(
       @Categories DataSync dataSync,
+      ProductRoomCategoryDao productRoomDao,
       CollectionSyncStateRepository collectionProductRepo,
       @Assisted Context context,
       @Assisted WorkerParameters workerParameters) {
     super(context, workerParameters);
     this.context = context;
     this.collectionProductRepo = collectionProductRepo;
+    this.productRoomCategoryDao = productRoomDao;
     this.dataSync = dataSync;
   }
 
@@ -43,6 +51,14 @@ public class ProductCategorySyncWorker extends Worker {
             .setContext(context)
             .setCollectionSyncStateRepository(collectionProductRepo)
             .setDataSync(dataSync)
+            .setStoreSyncData(documentSnapshots -> {
+              List<ProductCategory> productCategories = SyncRepository.toProductCategories(documentSnapshots);
+              for (ProductCategory productCategory : productCategories) {
+                ProductRoomCategory.Builder builder = ProductRoomCategory.builder();
+                builder.setProductCategory(productCategory);
+                productRoomCategoryDao.insert(builder.build());
+              }
+            })
             .build();
     return syncWorker.syncCollection();
   }
