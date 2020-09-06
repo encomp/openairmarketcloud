dfda250a4422f5a47a9dca0ba7e2219efe33483b
encomp <edgar.martinez.rico@gmail.com>
2020-09-05 23:49:27 -0700

    Define a new repository to retrieve all the measure units that are uncountable.

---
+++ app/src/main/java/com/toolinc/openairmarket/persistence/cloud/ProductMeasureUnitRepository.java
@@ -0,0 +1,53 @@
+package com.toolinc.openairmarket.persistence.cloud;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.toolinc.openairmarket.common.inject.Global;
+import com.toolinc.openairmarket.pos.persistence.model.product.ProductMeasureUnit;
+import java.util.List;
+import java.util.concurrent.Executor;
+import javax.inject.Inject;
+
+/**
+ * Measure unit repository hides firebase details of the api.
+ */
+public class ProductMeasureUnitRepository {
+
+  private final Executor executor;
+  private final FirebaseFirestore firebaseFirestore;
+
+  @Inject
+  public ProductMeasureUnitRepository(
+      @Global.NetworkIO Executor executor, FirebaseFirestore firebaseFirestore) {
+    this.executor = executor;
+    this.firebaseFirestore = firebaseFirestore;
+  }
+
+  public Task<QuerySnapshot> findAllUncountable(
+      OnSuccessListener<List<ProductMeasureUnit>> successListener,
+      OnFailureListener onFailureListener) {
+    return firebaseFirestore
+        .collection(CollectionsNames.PRODUCT_UNITS)
+        .whereEqualTo("countable", Boolean.FALSE)
+        .get()
+        .addOnSuccessListener(
+            executor, documentSnapshot -> onSuccess(documentSnapshot, successListener))
+        .addOnFailureListener(
+            executor,
+            exc -> {
+              onFailure(exc, onFailureListener);
+            });
+  }
+
+  private void onSuccess(
+      QuerySnapshot querySnapshot, OnSuccessListener<List<ProductMeasureUnit>> successListener) {
+    successListener.onSuccess(SyncRepository.toProductMeasureUnits(querySnapshot.getDocuments()));
+  }
+
+  private void onFailure(Exception exc, OnFailureListener onFailureListener) {
+    onFailureListener.onFailure(exc);
+  }
+}
